e29f3a8ec7e280585d36e8565cf113d72bd6501f
broetzel <brennenroetzel@gmail.com>
2024-06-18 13:17:22 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -44,3 +44,18 @@ function dosButtonPressed() {
 
 // Add event listener to Dos button
 document.querySelector('.dos-button').addEventListener('click', dosButtonPressed);
+
+// Function to change images and switch tab when Tres button is pressed
+function tresButtonPressed() {
+    // Change the image of Tres button
+    document.querySelector('.tres-button img').src = "https://i.imgur.com/lQyh44c.png";
+    // Change the image of Uno button
+    document.querySelector('.uno-button img').src = "https://i.imgur.com/yrdsbUV.png";
+    // Change the image of Dos button
+    document.querySelector('.dos-button img').src = "https://i.imgur.com/n0nJ0Uv.png";
+    // Switch to the SalesBrandTraining tab
+    openTab(null, 'SalesBrandTraining');
+}
+
+// Add event listener to Tres button
+document.querySelector('.tres-button').addEventListener('click', tresButtonPressed);
